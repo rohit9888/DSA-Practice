d36172f5fcb6cca07fc77395e2ea198c95c421fe
rohit9888 <rk_goyal@bt.iitr.ac.in>
2023-08-25 13:23:19 +0530

    Create validparenthesis.js

---
+++ validparenthesis.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    const dict = {
+        ")": "(",
+        "}": "{",
+        "]": "["
+    }
+    let stack = []; 
+    for (let c of s) { 
+        if (c === '(' || c === '{' || c === '[') { 
+            stack.push(c);
+        } else { 
+            if (stack.length && stack[stack.length - 1] !== dict[c])  {
+                console.log("helllo")
+                return false; 
+            }
+            stack.pop(); 
+        }
+    }
+    return !stack.length
+};
+console.log(isValid("()[]"))
